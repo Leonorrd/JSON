7a25e581393f6493154ae2c2ae17ff3d5f5f2149
Antonio Ruiz <antoniorg1974@outlook.com>
2017-03-10 12:30:26 +0100

    He creado fichero ejercicio3.py

---
+++ ejercicio3.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# _*_ coding: utf-8 
+#Importa fichero Json
+import json
+#lee fichero Json
+with open('monumentos.json') as data_file:    
+    doc = json.load(data_file)
+
+#Muestra el nombre monumentos y direcciónn.
+
+ejer = raw_input("número de ejercicio: ")
+#ejercicio 3
+if ejer == "3":
+    for nombre in doc:
+	print nombre["nombre"]
+        print nombre["direccion"]
+	print nombre["tipo"]
